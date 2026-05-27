bca791df67879a62a924e15406dce3dce4ce47c4
MEGALROCHA <rochamegal5@gmail.com>
2026-05-27 15:31:40 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+// Service Worker mínimo para instalar la App (PWA)
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
+});
